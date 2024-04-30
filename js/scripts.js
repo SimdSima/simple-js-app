7fbedc1ea9e26b3621ea155cf3572f5439f7d474
Sim <sima.pac@outlook.com>
2024-04-30 21:57:14 +0100

    Added Pokemon list and 3 types

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-alert('Hello world');
+let pokemonList = [
+0
+];
 
-let favoriteFood = 'Stir Fry';
-alert(favoriteFood);
-
-document.write(favoriteFood)
+let pokemonListarray = [
+  { name: 'Charizard', height: '1.7 m', types: 'Flame', Weight: '90.5 kg'},
+  { name: 'Blastoise', height: '1.6 m', types: 'Shell', Weight: '85.5 kg'},
+  { name: 'Ninetales', height: 1'.1 m', types: 'Fox ', Weight: '19.9 kg'},
+];

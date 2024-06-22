5848091ac2772ae84dce52442e801f7bf05e24cb
Sim <sima.pac@outlook.com>
2024-06-22 21:06:13 +0100

    Converting PokemonList to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,51 @@
-let pokemonList = [
-  { name: 'Charizard', height: 1.7, types: ['fire', 'flying'], weight: 90.5 },
-  { name: 'Blastoise', height: 1.6, types: ['water'], weight: 85.5 },
-  { name: 'Ninetales', height: 1.1, types: ['fire'], weight: 19.9 }
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying'],
+    },
+    {
+      name: 'Blastoise',
+
+      height: 1.6,
+      types: ['water'],
+    },
+    {
+      name: 'Ninetales',
+      height: 1.1,
+      types: ['fire'],
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+    // Checking if added Pokemon Item to a list is an Object
+    if (typeof pokemon === 'object')
+      return true
+  }
+    else {
+  console.log('Pokemn type is not an object') }
+
+function getAll() {
+  return pokemonList;
+}
+
+return {
+  add: add,
+  getAll: getAll
+};
+
+pokemonList.forEach(function (pokemon) {
+  console.log(pokemon.name + ' is ' + pokemon.height + ' tall.')
+});
+
+} ) ();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Charizard' });
+console.log(pokemonRepository.getAll());
+
 
 // Set thresholds for big and small Pokémon
 const bigHeightThreshold = 1.5;
@@ -19,8 +62,8 @@ for (let i = 0; i < pokemonList.length; i++) {
     message = "Average size Pokémon";
   }
   else {
-     // Pokémon is considered small if its height is less than or equal to smallHeightThreshold
-     message = "Wow, so small!";
+    // Pokémon is considered small if its height is less than or equal to smallHeightThreshold
+    message = "Wow, so small!";
   }
 
   // Output the information

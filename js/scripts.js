628c51b20b0568e15a7de30391ebc961c87d44a6
Sim <sima.pac@outlook.com>
2024-06-23 17:01:54 +0100

    Fixed forEach code

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ let pokemonRepository = (function () {
     },
     {
       name: 'Blastoise',
-
       height: 1.6,
       types: ['water'],
     },
@@ -19,39 +18,43 @@ let pokemonRepository = (function () {
   ];
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
     // Checking if added Pokemon Item to a list is an Object
-    if (typeof pokemon === 'object')
+    if (typeof pokemon === 'object' && 'name' in pokemon
+      && 'height' in pokemon && 'types' in pokemon) {
+      pokemonList.push(pokemon);
       return true
+    } else {
+      console.log('Pokemn type is not an object or missing required properties');
+      return false;
+    }
   }
-    else {
-  console.log('Pokemn type is not an object') }
 
-function getAll() {
-  return pokemonList;
-}
+  function getAll() {
+    return pokemonList;
+  }
 
-return {
-  add: add,
-  getAll: getAll
-};
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
-pokemonList.forEach(function (pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + ' tall.')
+// Using the getAll method to get the list of Pokemon
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  console.log(pokemon.name + ' is ' + pokemon.height + ' tall.');
 });
 
-} ) ();
-
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Charizard' });
+pokemonRepository.add({ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] });
 console.log(pokemonRepository.getAll());
-
 
 // Set thresholds for big and small Pokémon
 const bigHeightThreshold = 1.5;
 const smallHeightThreshold = 0.5;
 
-for (let i = 0; i < pokemonList.length; i++) {
+// Using the getAll method to iterate through the Pokemon list
+pokemonRepository.getAll().forEach(function (pokemon) {
   let message = "";
   if (pokemonList[i].height > bigHeightThreshold) {
     // Pokémon is considered big if its height is greater than the bigHeightThreshold
@@ -65,7 +68,9 @@ for (let i = 0; i < pokemonList.length; i++) {
     // Pokémon is considered small if its height is less than or equal to smallHeightThreshold
     message = "Wow, so small!";
   }
+}
 
   // Output the information
-  document.write("<p class='" + (pokemonList[i].height > bigHeightThreshold ? "big" : (pokemonList[i].height <= bigHeightThreshold && pokemonList[i].height > smallHeightThreshold ? "average" : "small")) + "'>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - " + message + "</p>");
-}
+  document.write("<p class='" + (pokemon.height > bigHeightThreshold ? "big" :
+  (pokemon.height <= bigHeightThreshold && pokemon.height > smallHeightThreshold ? "average" : "small"))
+  + "'>" + pokemon.name + " (height: " + pokemon.height + ") - " + message + "</p>"))

051133c8f4874dc09d010548c8bab43db443d962
Sim <sima.pac@outlook.com>
2024-09-16 00:44:29 +0100

    Using the getAll method to get the list of Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -27,44 +27,41 @@ let pokemonRepository = (function () {
       console.log('Pokemon type is not an object or missing required properties');
       return false;
     }
+
   }
 
   function getAll() {
     return pokemonList;
   }
 
+  function addListItem(pokemon) {
+    // Adds the button with pokemn list
+    let pokemonList = document.querySelector ('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = console.log(pokemon.name);
+    pokemon - list.appendChild(button);
+    button.classList.add('button-class');
+    listpokemon.appendChild(button);
+    pokemonlist.appendChild(listpokemon);
+  };
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
-})();
 
-// Using the getAll method to get the list of Pokemon
-pokemonRepository.getAll().forEach(function (pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + ' tall.');
-});
+})();
 
 console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] });
+
 console.log(pokemonRepository.getAll());
 
-// Set thresholds for big and small Pokémon
-const bigHeightThreshold = 1.5;
-const smallHeightThreshold = 0.5;
-
-// Using the getAll method to iterate through the Pokemon list
+// Using the getAll method to get the list of Pokemon
 pokemonRepository.getAll().forEach(function (pokemon) {
-  let message = "";
-  if (pokemon.height > bigHeightThreshold) {
-    message = "Wow, that's big!";
-  } else if (pokemon.height <= bigHeightThreshold && pokemon.height > smallHeightThreshold) {
-    message = "Average size Pokémon";
-  } else {
-    message = "Wow, so small!";
-  }
+  pokemonRepository.addListItem(pokemon);
+}
 
-  // Output the information
-  document.write("<p class='" + (pokemon.height > bigHeightThreshold ? "big" :
-    (pokemon.height <= bigHeightThreshold && pokemon.height > smallHeightThreshold ? "average" : "small"))
-    + "'>" + pokemon.name + " (height: " + pokemon.height + ") - " + message + "</p>");
-  });
+);

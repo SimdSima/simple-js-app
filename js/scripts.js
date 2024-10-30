2cbac90fbcd24ca244872c20b718119de4096f38
Sim <sima.pac@outlook.com>
2024-10-30 09:39:40 +0000

    Added functions and the Pokémon API
    Added a list of Pokémon from an external data source (the Pokémon API) using fetch function. Added functions loadDetails & showDetails

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 let pokemonList = [
+  // pokemon objects
   { name: 'Charizard', height: 1.7, types: ['fire', 'flying'], weight: 90.5 },
   { name: 'Blastoise', height: 1.6, types: ['water'], weight: 85.5 },
   { name: 'Ninetales', height: 1.1, types: ['fire'], weight: 19.9 }
+];
+
+let pokemonList2 = [
+  // different set of Pokémon objects
+  { name: 'Charizard2', height: 1.7, types: ['fire', 'flying'], weight: 90.5 }
 ];
 
 // Set thresholds for big and small Pokémon
 const bigHeightThreshold = 1.5;
 const smallHeightThreshold = 0.5;
 
-for (let i = 0; i < pokemonList.length; i++) {
+// printArrayDetails function declaration
+function printArrayDetails(list){
+  for (let i = 0; i < pokemonList.length; i++) {
   let message = "";
   if (pokemonList[i].height > bigHeightThreshold) {
     // Pokémon is considered big if its height is greater than the bigHeightThreshold
@@ -22,7 +30,14 @@ for (let i = 0; i < pokemonList.length; i++) {
      // Pokémon is considered small if its height is less than or equal to smallHeightThreshold
      message = "Wow, so small!";
   }
+}
 
   // Output the information
   document.write("<p class='" + (pokemonList[i].height > bigHeightThreshold ? "big" : (pokemonList[i].height <= bigHeightThreshold && pokemonList[i].height > smallHeightThreshold ? "average" : "small")) + "'>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") - " + message + "</p>");
 }
+
+printArrayDetails(pokemonList); 
+// executes the function using ‘pokemonList‘ as its input
+
+printArrayDetails(pokemonList2); 
+// executes the function using ‘pokemonList2‘ as its input

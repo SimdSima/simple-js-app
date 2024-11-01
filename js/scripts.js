76273b2b647135574e3d0353f6f558598f94ff0f
Sim <sima.pac@outlook.com>
2024-11-01 18:55:35 +0000

    Fixed errors
    Fixed API link to load pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,15 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: 'Charizard',
-      height: 1.7,
-      types: ['fire', 'flying'],
-    },
-    {
-      name: 'Blastoise',
-      height: 1.6,
-      types: ['water'],
-    },
-    {
-      name: 'Ninetales',
-      height: 1.1,
-      types: ['fire'],
-    }
-  ];
+  let pokemonList = [];
+  // Initializes an empty list to store Pokémon
 
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  // The API URL to get the first 150 Pokémon
+
 
   function add(pokemon) {
     // Checking if added Pokemon Item to a list is an Object
     if (typeof pokemon === 'object' && 'name' in pokemon
-      && 'height' in pokemon && 'types' in pokemon) {
+      && 'detailsUrl' in pokemon) {
       pokemonList.push(pokemon);
       return true;
     } else {
@@ -37,23 +24,22 @@ let pokemonRepository = (function () {
 
   function addListItem(pokemon) {
     // Adds the button with pokemn list
-    let pokemonList = document.querySelector('.pokemonlist');
+    let pokemonListElement = document.querySelector('.pokemonlist');
     // 'pokemonList' refers to the <ul>
-    let listpokemon = document.createElement('li');
+    let listItem = document.createElement('li');
     let button = document.createElement('button');
     // This will set the Pokémon name on the button
     button.innerText = pokemon.name;
     button.classList.add('button-class');
     // Append the list item to the <ul>
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
+    listItem.appendChild(button);
+    pokemonListElement.appendChild(listItem);
 
     //event listener to the button
     button.addEventListener('click', function () {
       showLoadingMessage();
       loadDetails(pokemon).then(function () {
         showDetails(pokemon);
-        // Ensure this calls the correct showDetails function
         // When the button is clicked, show details
         hideLoadingMessage();
       }).catch(function (error) {
@@ -63,81 +49,78 @@ let pokemonRepository = (function () {
     });
   }
 
-    function loadList() {
-      showLoadingMessage();
-      return fetch(apiUrl).then(function (response) {
-        return response.json();
-      }).then(function (json) {
-        json.results.forEach(function (item) {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url
-          };
-          add(pokemon);
-        });
-        hideLoadingMessage();
-      }).catch(function (e) {
-        console.error(e);
-        hideLoadingMessage();
-      })
-    }
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+      hideLoadingMessage();
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    })
+  }
 
-    function loadDetails(item) {
-      let url = item.detailsUrl;
-      return fetch(url).then(function (response) {
-        return response.json();
-      }).then(function (details) {
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(response => response.json())
+      .then(details => {
         // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
-        item.types = details.types;
-      }).catch(function (e) {
-        console.error(e);
+        item.types = details.types.map(type => type.type.name);
+      }).catch(error => {
+        console.error("Error loading details:", error);
       });
-    }
+  }
 
-    function showLoadingMessage() {
-      console.log("Loading... Please wait.");
-      // Additional code to display a loading message in the UI can be added here
-    }
+  function showLoadingMessage() {
+    console.log("Loading... Please wait.");
+    // Additional code to display a loading message in the UI can be added here
+  }
 
-    function hideLoadingMessage() {
-      console.log("Loading complete.");
-      // Additional code to hide the loading message in the UI can be added here
-    }
+  function hideLoadingMessage() {
+    console.log("Loading complete.");
+    // Additional code to hide the loading message in the UI can be added here
+  }
 
-    // Call loadList after defining all functions
-    loadList().then(function () {
-      getAll().forEach(function (pokemon) {
-        addListItem(pokemon);
-      });
+  // Call loadList after defining all functions
+  loadList().then(() => {
+    getAll().forEach(pokemon => {
+      addListItem(pokemon);
     });
+  });
 
-    return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem,
-      loadList: loadList,
-      loadDetails: loadDetails,
-      showDetails: showDetails
-    };
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
 })();
-
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] });
 
 console.log(pokemonRepository.getAll());
 
 function showDetails(pokemon) {
-  loadDetails(pokemon).then(function () {
+  loadDetails(pokemon).then(() => {
     console.log(pokemon);
+    // Logs details to the console
     // Additional code to display Pokémon details can be added here
   });
 }
 
-pokemonRepository.loadList().then(function() {
+pokemonRepository.loadList().then(function () {
   // Now the data is loaded!
-  pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
